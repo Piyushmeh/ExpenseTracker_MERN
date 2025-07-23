1955f7b7aebbe3478bed486d232fb5ec193fa35f
Piyush <piyushbansod006@gmail.com>
2025-07-23 19:04:59 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
+const app = express();
+
+const PORT = process.env.PORT;
+console.log('PORT:', PORT);
+
+app.use(express.json());
+app.use(cors());
+
+readdirSync('./routes').map((route) =>
+  app.use('/api/v1', require('./routes/' + route))
+);
+
+const server = () => {
+  db();
+  app.listen(PORT, () => {
+    console.log('listening to port:', PORT);
+  });
+};
+
+server();
